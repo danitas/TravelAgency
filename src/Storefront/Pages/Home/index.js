b74101173719dfd70798b056876e314438d9d2a2
danitas <danitas.bonitas@gmail.com>
2022-02-02 16:49:13 +0200

    created structure -pages -components

---
+++ src/Storefront/Pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import MainSlider from './Storefront/Components/Home/MainSlider';
+// import AroundWorld from './Storefront/Components/Home/AroundWorld';
+
+const Home = () => {
+    return (
+        <div>
+            {/* <MainSlider />
+            <AroundWorld /> */}
+        </div>
+    );
+}
+
+export default Home;
